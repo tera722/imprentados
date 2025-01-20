a3c525036df7572e1a3857953fd11710e1a1de76
Tuxed-Cat <lblopez51@gmail.com>
2025-01-19 21:46:47 -0600

    Agregue una funcion simple de validacion en el ver.java para el boton de eliminar

--- app/src/main/java/com/example/imprentados/ver.java
+++ app/src/main/java/com/example/imprentados/ver.java
@@ -1,9 +1,35 @@
 package com.example.imprentados;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class ver extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.ver);
 
+        Button ButtonDel = findViewById(R.id.BtnDelete);
+        ButtonDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Toast.makeText(ver.this, "Hola we", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
 }
